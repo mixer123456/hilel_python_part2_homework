d20cca7ce312b9a9807bf3fc17491c09bb22194a
mixer123456 <maxiepishkin@gmail.com>
2024-09-29 09:17:58 +0100

    3-4 homework

--- homework3/homework.py
+++ homework3/homework.py
@@ -2,6 +2,11 @@
 list1 = [1, 10, 4, 13, 22, 10, 0, 105, 12, 11, 105]
 
 def get_largest_even_number(lst: list) -> int:
+    '''
+    function get list and return largest even number
+    :param lst: function get list
+    :return: largest even number
+    '''
     max_even_number = [el for el in lst if el % 2 == 0]
 
     return max(max_even_number)
@@ -31,6 +36,12 @@ list3 = [1, 10, 4, 13, 22, 10, 0, 100, 12, 14, 105]
 list4 = [1, 1, 3, 13, 22, 5, 17]
 
 def compare_list(lst1: list, lst2: list):
+    '''
+    function get 2 lists and compares them
+    :param lst1: 1 list
+    :param lst2: 2 list
+    :return: True or False
+    '''
     even_list_1 = [el for el in lst1 if el % 2 == 0]
     odd_list_2 = [el for el in lst2 if el % 2 != 0]
 

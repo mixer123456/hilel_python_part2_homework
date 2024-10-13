1d6e5115d90f7014e3b7750a8652730ccf8296ae
mixer123456 <maxiepishkin@gmail.com>
2024-10-13 17:14:10 +0100

    7 homework

--- homework7/homework.py
+++ homework7/homework.py
@@ -16,6 +16,25 @@ def read_file(file_path: str):
         return file.read()
 
 
+def get_most_popular_letters_in_text(letters: list, text: str, limit: int = 1):
+    '''
+    get most  popular letters in text
+    :param letters: our letters for counting
+    :param text: our text for searching letters
+    :param limit: limit for top
+    :return: return top popular letters in text
+    '''
+    result = {}
+
+    for char in letters:
+        result[char] = text.count(char)  # рахуємо кількість літер у тексті та добавляємо у словник ці літер
+    result = list(result.items())  # перетворюємо у ліст щоб можно було йогоо соортувати
+    result.sort(reverse=True, key=lambda char: char[1])  # сортуємо за допомогою анонімною фунції
+    result = result[:limit]  # робимо тлп найпоопулярніших літер
+
+    return [char[0] for char in result]  # перетворрюємо список таплів у список символів
+
+
 def func(file_path: str):
     '''
     calculate count of vowels and count of consonants letters in text and if vowels letters count bigger print top 3
@@ -24,22 +43,35 @@ def func(file_path: str):
     :return: return top 3 popular vowels letter or most popular consonants letter
     '''
     text = read_file(file_path).lower()
-    result = {}
     text_vowels = [char for char in text if char in vowels_letters]
     text_consonants = [char for char in text if char in consonants_letters]
     vowels_letters_count = len(text_vowels)
     consonants_letters_count = len(text_consonants)
 
     if vowels_letters_count > consonants_letters_count:
-        for char in vowels_letters:
-            result[char] = text.count(char)
-        result = list(result.items())
-        result.sort(reverse=True, key=lambda char: char[1])
-        return result[:3]
+        return get_most_popular_letters_in_text(vowels_letters, text, 3)
 
     if vowels_letters_count < consonants_letters_count:
-        for char in consonants_letters:
-            result[char] = text.count(char)
-        result = list(result.items())
-        result.sort(reverse=True, key=lambda char: char[1])
-        return result[0]
+        return get_most_popular_letters_in_text(consonants_letters, text)
+
+    return 'кількість голоосних дооріввнює кількості приголосних'
+
+
+def safe_result(file_name: str, result: str):
+    '''
+    safe result to new file
+    :param file_name: file name
+    :param result: result thhat we want safe
+    :return: nothing
+    '''
+    with open(file_name, mode='w') as file:
+        file.write(result)
+
+
+top_letters = func('input_8.txt')
+print(top_letters)
+
+safe_result('output.txt', top_letters[0])
+
+with open('output.txt', mode="w") as file:
+    file.write(top_letters[0])

3de654cf14070c3e6e91431a84df264b305c6a45
mixer123456 <maxiepishkin@gmail.com>
2024-10-20 10:46:15 +0100

    8 homework

--- homework8/homework.py
+++ homework8/homework.py
@@ -1,3 +1,6 @@
+import re
+
+
 def read_file(file_path: str):
     '''
     read file
@@ -8,60 +11,64 @@ def read_file(file_path: str):
         return file.read()
 
 
-def func(file_path: str):
+def find_most_popular_and_shortest_word(file_path: str):
     '''
     find the most popular and the shortest word in the file
     :param file_path: file name
     :return: return list tuple with the most popular and the shortest word in file
     '''
-    text = read_file(file_path).lower().replace('.', '').replace(',', '').replace('!', '').replace('?', '').split()
 
-    #утворюємо сет унікальних слів
+    text = read_file(file_path)
+    text = re.findall(r'\b\w+\b', text.lower())  # використовуємо для розбиття текста на слова з умовою використування символів пунктуації
+
+
+    # утворюємо сет унікальних слів
     set_text = set(text)
 
-    #перетворюємо його у ліст та сортуруємо його за довжиною слів
+    # перетворюємо його у ліст та сортуруємо його за довжиною слів
     list_text = list(set_text)
     list_text.sort(key=len)
 
-    #берем 1-ий елемент ліста та його довжину
+    # берем 1-ий елемент ліста та його довжину
     len_first_el = len(list_text[0])
 
-    #ввидаляємо всі елементи із ліста які довші за вказаний
+    # ввидаляємо всі елементи із ліста які довші за вказаний
     list_text = list(filter(lambda el: len_first_el == len(el), list_text))
 
     # утвворюємо дікт результатів
     dict_text = {}
 
-    #ідем фором по лісту та дообавляєм дикт ключ значення ключ це слово значение це число
+    # ідем фором по лісту та дообавляєм дикт ключ значення ключ це слово значение це число
     for el in list_text:
         dict_text[el] = text.count(el)
 
-    #з нашего дікта робим список таплів та сортируєм йогоо по кількості
+    # з нашего дікта робим список таплів та сортируєм йогоо по кількості
     list_text = list(dict_text.items())
     list_text.sort(reverse=True, key=lambda el: el[1])
 
-    #ввидалити з нашого списка таплів ввсі елементи кількість поовторень яких меньше ніж у першого
+    # ввидалити з нашого списка таплів ввсі елементи кількість поовторень яких меньше ніж у першого
     count_first_el = list_text[0][1]
     list_text = list(filter(lambda el: count_first_el == el[1], list_text))
 
     return list_text
 
 
-def func2(file_path: str):
+def convert_from_lowercase_to_uppercase(file_path: str):
     '''
     convert the most popular and the shortest word in the file from lowercase to uppercase
     :param file_path: file name
     :return: returns file in which the most popular and the shortest word is in uppercase
     '''
-    text_original = read_file(file_path)
-    text = text_original.replace('.', '').replace(',', '').split()
-    list_text = func(file_path)
-    list_text = [el[0] for el in list_text]
-    for el in text:
-        if el.lower() in list_text:
-            text_original = text_original.replace(f' {el} ', f' {el.upper()} ')
-    return text_original
+    text = read_file(file_path)
+    words_list = find_most_popular_and_shortest_word(file_path)
+
+    for el in words_list:
+        word = el[0]
+        pattern = r'\b' + re.escape(word) + r'\b'
+        text = re.sub(pattern, word.upper(), text, flags=re.IGNORECASE)
+
+    return text
 
 
-res = func2('input_1.txt')
+res = convert_from_lowercase_to_uppercase('input_1.txt')
 print(res)

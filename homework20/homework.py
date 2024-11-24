0d9f612b8ebe3e9ba4eba46b9e619a7a4965e884
mixer123456 <maxiepishkin@gmail.com>
2024-11-24 15:56:24 +0000

    20 homework

--- homework20/homework.py
+++ homework20/homework.py
@@ -5,13 +5,12 @@ with sqlite3.connect('Chinook_Sqlite.sqlite') as con:
     cur.execute('''SELECT * FROM Customer WHERE LENGTH(Company) == (SELECT MAX(LENGTH(Company)) FROM Customer)''')
     cur.execute('''SELECT COUNT(*) AS CustomerCount FROM Customer WHERE Company IS NULL AND Fax IS NULL''')
     cur.execute("""SELECT CASE
-        WHEN Country IN ('USA', 'Canada', 'Mexico') THEN 'North America'
-        WHEN Country IN ('France', 'Germany', 'Italy', 'Spain', 'United Kingdom', 
-                         'Netherlands', 'Sweden', 'Norway', 'Switzerland', 'Poland') THEN 'Europe'
-        WHEN Country IN ('Australia', 'New Zealand') THEN 'Australia'
-        WHEN Country IN ('Brazil', 'Argentina', 'Chile', 'Colombia') THEN 'South America'
-        WHEN Country IN ('China', 'India', 'Japan', 'South Korea') THEN 'Asia'
-        WHEN Country IN ('South Africa', 'Egypt', 'Nigeria') THEN 'Africa'
+        WHEN Phone LIKE '+1%' THEN 'North America'
+        WHEN Phone LIKE '+2%' THEN 'Africa'
+        WHEN Phone LIKE '+3%' OR Phone LIKE '+4%' THEN 'Europe'
+        WHEN Phone LIKE '+5%' THEN 'South America'
+        WHEN Phone LIKE '+6%' THEN 'Australia'
+        WHEN Phone LIKE '+8%' OR Phone LIKE '+9%' THEN 'Asia'
         ELSE 'Unknown'
-    END AS Continent, COUNT(*) AS CustomerCount FROM Customer GROUP BY Continent""")
+    END AS Continent, COUNT(*) AS CustomerCount FROM Customer GROUP BY Continent;""")
     print(*cur.fetchall(), sep='\n')

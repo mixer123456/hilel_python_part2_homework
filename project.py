884da73372e41db8182a1ffd454af703daeb9ad7
mixer123456 <maxiepishkin@gmail.com>
2025-01-08 13:39:31 +0000

    project

--- project.py
+++ project.py
@@ -3,21 +3,23 @@ from datetime import date
 
 db = SqliteDatabase('Briefcase.db')
 
+
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Transaction(BaseModel):
     IdTransaction = AutoField(primary_key=True)
     Date = DateField(default=date.today, null=False)
-    TransactionType = CharField(null=False) # withdraw, deposit, buy or sell
+    TransactionType = CharField(null=False)  # withdraw, deposit, buy or sell
     Amount = IntegerField(null=False, default=0)
     Token = CharField(null=False)
     Price = IntegerField(null=False, default=0)
     Balance = IntegerField(null=False, default=0)
     IdParentTransaction = IntegerField()
 
-    def add_transaction(transaction_type: str, amount: int, token: str, parent_transaction: int = None):
+    def add_transaction(transaction_type: str, amount: float, token: str, parent_transaction: int = None):
         try:
             # Get the latest balance for the token, if it exists
             last_transaction = (Transaction
@@ -68,17 +70,48 @@ class Transaction(BaseModel):
 with db:
     db.create_tables([Transaction])
 
-customer = {'TransactionType': 'deposit', 'Amount': 1, 'Token': 'bread'.lower(), 'Balance': 3}
-Transaction.add_transaction('deposit', 100, 'euro')
-Transaction.show_balance_by_token('euro')
+
+# customer = {'TransactionType': 'deposit', 'Amount': 1, 'Token': 'bread'.lower(), 'Balance': 3}
+# Transaction.add_transaction('deposit', 100, 'euro')
+# Transaction.show_balance_by_token('euro')
 
 
+class Portfolio:
+    def __init__(self, name: str, money: int):
+        self.name = name
+        self.money = money
 
+    def deposit(self, amount: float, token: str):
+        if amount <= 0:
+            raise Exception('You want to deposit nothing?')
 
+        Transaction.add_transaction('deposit', amount, token)
 
+    def withdraw(self, amount: float, token: str):
+        if amount <= 0:
+            raise Exception('You want to withdraw nothing?')
 
+        Transaction.add_transaction('withdraw', amount, token)
 
+    def buy(self, token: str, price: int, amount: float, parent_transaction: int):
+        if amount <= 0:
+            raise Exception('You want to buy nothing?')
 
+        if price <= 0:
+            raise Exception('Token cant be free or less than 0')
 
+        if price >= self.money:
+            raise Exception('You dont have enough money')
 
+        Transaction.add_transaction('buy', amount, token, parent_transaction)
+        self.money = - price
 
+    def sell(self, token: str, price: int, amount: float, parent_transaction: int):
+        if amount <= 0:
+            raise Exception('You want to sell nothing?')
+
+        if price <= 0:
+            raise Exception('Token cant be free or less than 0')
+
+        Transaction.add_transaction('sell', amount, token, parent_transaction)
+        self.money += price
